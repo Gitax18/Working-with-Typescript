abd1cd180d97bfb2f5e93546573d94913236522b
Gitax18 <lusifer0418@gmail.com>
2025-01-19 17:31:35 +0530

    change the identifier of Company class name

--- 07-project-1/src/Classes/class.Company.ts
+++ 07-project-1/src/Classes/class.Company.ts
@@ -1,12 +1,12 @@
 import { faker } from "@faker-js/faker";
 
 export class Company {
-  companyName: string;
+  name: string;
   catchPhrase: string;
   location: { lat: number; lng: number };
 
   constructor() {
-    this.companyName = faker.company.name();
+    this.name = faker.company.name();
     this.catchPhrase = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
@@ -14,6 +14,6 @@ export class Company {
     };
   }
   showSummary(): string {
-    return `Name: ${this.companyName}\nLat: ${this.location.lat}\nLng: ${this.location.lng}`;
+    return `Name: ${this.name}\nLat: ${this.location.lat}\nLng: ${this.location.lng}`;
   }
 }
