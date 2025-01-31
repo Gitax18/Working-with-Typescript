52d22a59355b75607a44e213fc142fd5135d41a1
Gitax18 <lusifer0418@gmail.com>
2025-01-31 20:18:24 +0530

    07-show-basic-map

--- 07-project-1/src/index.ts
+++ 07-project-1/src/index.ts
@@ -5,6 +5,10 @@ import {} from "google.maps";
 const user = new User();
 const company = new Company();
 
-console.log(user.showSummary());
-console.log(company.showSummary());
-console.log(google);
+const div = document.getElementById("map") as HTMLElement;
+
+console.log(div);
+new google.maps.Map(div, {
+  zoom: 8,
+  center: { lat: user.location.lat, lng: user.location.lng },
+});
