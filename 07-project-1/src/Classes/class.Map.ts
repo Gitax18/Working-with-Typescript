bd39e97b881d92d9e0105fa59ee95636a1e738ac
Gitax18 <lusifer0418@gmail.com>
2025-02-01 00:04:11 +0530

    07-add-map-markers

--- 07-project-1/src/Classes/class.Map.ts
+++ 07-project-1/src/Classes/class.Map.ts
@@ -1,4 +1,6 @@
 import {} from "google.maps";
+import { User } from "./class.User";
+import { Company } from "./class.Company";
 
 interface MapOptions {
   zoom: number;
@@ -8,10 +10,35 @@ interface MapOptions {
   };
 }
 
+interface MarkerOptions {
+  map: google.maps.Map;
+  position: { lat: number; lng: number };
+  label: string;
+}
+
 export class Map {
   private googleMap: google.maps.Map;
+  private userMarker: google.maps.Marker;
+  private companyMarker: google.maps.Marker;
 
   constructor(element: HTMLElement, opts: MapOptions) {
     this.googleMap = new google.maps.Map(element, opts);
+  }
+
+  addMarker(entity: User | Company, entityMarker: google.maps.Marker): void {
+    const opts: MarkerOptions = {
+      map: this.googleMap,
+      position: entity.location,
+      label: entity.name,
+    };
+    entityMarker = new google.maps.Marker(opts);
+  }
+
+  addUserMarker(user: User): void {
+    this.addMarker(user, this.userMarker);
+  }
+
+  addCompanyMarker(company: Company): void {
+    this.addMarker(company, this.companyMarker);
   }
 }
--- 07-project-1/src/index.ts
+++ 07-project-1/src/index.ts
@@ -5,7 +5,10 @@ import { Map } from "./Classes/class.Map";
 const user = new User();
 const company = new Company();
 const div = document.getElementById("map") as HTMLElement;
-new Map(div, {
-  zoom: 3,
-  center: { lat: 90, lng: 180 },
+const MyMap = new Map(div, {
+  zoom: 2,
+  center: { lat: 0, lng: 0 },
 });
+
+const userMarker = MyMap.addUserMarker(user);
+const companyMarker = MyMap.addCompanyMarker(company);
