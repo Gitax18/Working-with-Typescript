039ae18935b272edb919c58846e8c4a897d74858
Gitax18 <lusifer0418@gmail.com>
2025-01-31 15:45:15 +0530

    07-add-gmap-types

--- 07-project-1/src/index.ts
+++ 07-project-1/src/index.ts
@@ -1,8 +1,10 @@
 import { User } from "./Classes/class.User";
 import { Company } from "./Classes/class.Company";
+import {} from "google.maps";
 
 const user = new User();
 const company = new Company();
 
 console.log(user.showSummary());
 console.log(company.showSummary());
+console.log(google);
