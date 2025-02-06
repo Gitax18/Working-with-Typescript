b7e27b8811b6b3e63fb272d3137a67ee586420de
Gitax18 <lusifer0418@gmail.com>
2025-02-07 00:39:28 +0530

    08-basic-refactor-string-example

--- 08-project-2/src/index.ts
+++ 08-project-2/src/index.ts
@@ -1,9 +1,25 @@
 import { CollectionNumber } from "./classes/Collections/Collection.Numbers";
+import { CollectionString } from "./classes/Collections/Collection.String";
 import Sorter from "./classes/Sorter";
 
 const data = new CollectionNumber([
   1, 2, 3, 7, 4, 2, 89, 32, 11, 51, 2, 5, 896, 23, 0,
 ]);
 
-const SortedData = new Sorter(data);
-SortedData.show();
+const name = new CollectionString("GiTansHU");
+
+const sortedData = new Sorter(data);
+const sortedName = new Sorter(name);
+// data before sorting
+data.show();
+// performing sorting
+sortedData.sort();
+// data after sorting
+data.show();
+
+// data before sorting
+name.show();
+// performing sorting
+sortedName.sort();
+// data after sorting
+name.show();
