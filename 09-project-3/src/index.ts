f8e4072b8b68395a5e159f16676b845e9771f1a5
Gitax18 <lusifer0418@gmail.com>
2025-02-09 22:26:09 +0530

    09-brute-force-approach

--- 09-project-3/src/index.ts
+++ 09-project-3/src/index.ts
@@ -1,1 +1,17 @@
-console.log("hello world!!!!");
+import fs from "fs";
+
+const matches = fs
+  // reading file
+  .readFileSync("football.csv", { encoding: "utf-8" })
+  // splitting string: string[]
+  .split("\n")
+  // converting array of string to array of string array's: string[][]
+  .map((row: string): string[] => row.split(","));
+
+// lets count how many matches wins my manchester united team as Home and Away team.
+let manUnitedWins = 0;
+for (let i = 0; i < matches.length; i++) {
+  if (matches[i][1] == "Man United" && matches[i][5] == "H") manUnitedWins++;
+  if (matches[i][2] == "Man United" && matches[i][5] == "A") manUnitedWins++;
+}
+console.log("Man United has won", manUnitedWins, "games");
