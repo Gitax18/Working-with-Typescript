be56027a37562a90bc3fa5eb2a24b60fdc2f5cc8
Gitax18 <lusifer0418@gmail.com>
2025-02-09 22:40:49 +0530

    09-implement-enum-for-result-status

--- 09-project-3/src/index.ts
+++ 09-project-3/src/index.ts
@@ -9,9 +9,18 @@ const matches = fs
   .map((row: string): string[] => row.split(","));
 
 // lets count how many matches wins my manchester united team as Home and Away team.
-let manUnitedWins = 0;
-for (let i = 0; i < matches.length; i++) {
-  if (matches[i][1] == "Man United" && matches[i][5] == "H") manUnitedWins++;
-  if (matches[i][2] == "Man United" && matches[i][5] == "A") manUnitedWins++;
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
 }
-console.log("Man United has won", manUnitedWins, "games");
+
+let manUnitedWins = 0;
+
+for (let i = 0; i < matches.length; i++) {
+  if (matches[i][1] == "Man United" && matches[i][5] == MatchResult.HomeWin)
+    manUnitedWins++;
+  if (matches[i][2] == "Man United" && matches[i][5] == MatchResult.AwayWin)
+    manUnitedWins++;
+}
+console.log("Man United has won", manUnitedWins, "games !!!");
